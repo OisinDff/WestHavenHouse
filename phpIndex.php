1e078d00fa3c96efaa00bbbd6cce34de660a7843
OisinDff <OisinDff@gmail.com>
2025-02-14 12:47:18 +0000

    WestHavenHouse
    Version 1/ CSS working but new changes to css not working for some reason

---
+++ phpIndex.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" type="text/css" href="/css/stylesheet.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <title>Test</title>
+</head>
+
+<header class="header">
+  <h1>West Heaven House</h1>
+</header>
+<p class="location"><i class="fa fa-home" style="font-size:24px; padding-right:5px"></i>Doolin Toomilin, Doolin, Ireland
+</p>
+
+
+
+<body>
+  <div class="container">
+    <div class="navBar">
+      <input type="text" placeholder="Search..">
+    </div>
+
+    <div class="recommendedContainer">
+      <div>
+        <img class="recommendedRoom" src="./img/recommendedRoom.jpg" alt="Recommended Room" />
+      </div>
+    </div>
+
+    <div class="flexContainer">
+      <div>
+        <img class="room2" src="./img/room2.jpg" alt="room2" />
+
+        <p class="roomText">Room Description</p>
+      </div>
+
+      <div>
+        <img class="room2" src="./img/room3.jpg" alt="room2" />
+        <p class="roomText">Room Description</p>
+      </div>
+
+      <div>
+        <img class="room2" src="./img/room4.png" alt="room2" />
+        <p class="roomText">Room Description</p>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
