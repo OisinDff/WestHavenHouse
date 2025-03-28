ddb5cf29d767c78a6159b635ad56cceef5b3d34e
amyoneill <b00161543@mytudublin.ie>
2025-03-28 12:03:56 +0000

    Add files via upload

---
+++ header.php
@@ -0,0 +1,18 @@
+<header class="header">
+    <h1>West Heaven House</h1>
+</header>
+<p class="location"><i class="fa fa-home" style="font-size:24px; padding-right:5px"></i>Doolin Toomilin, Doolin, Ireland</p>
+
+<div class="topnav">
+    <a href="/index.php">Home</a>
+    <a href="/BookYourStay.php">Book Your Stay</a>
+    <a href="/AboutUs.php">About Us</a>
+    <a href="/Contact.php">Contact Us</a>
+    <a href="/Search.php">Search</a>
+    <div class="search-container">
+        <form action="/action_page.php">
+            <input type="text" placeholder="Search.." name="search">
+            <button type="submit"><i class="fa fa-search"></i></button>
+        </form>
+    </div>
+</div>
